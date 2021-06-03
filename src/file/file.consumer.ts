cd3ef0b1703f3baf997b7f59dfc557e0bbbdd873
Kalaev Viktor <kalaev-viktor@mail.ru>
2021-06-03 22:04:38 +0300

    правки по комментариям

--- src/file/file.consumer.ts
+++ src/file/file.consumer.ts
@@ -28,7 +28,8 @@ export class FileConsumer {
     try {
       const filePath = path.resolve(__dirname, '..', '..', 'xsl');
       const file = await this.fileService.findById(Number(job.id));
-      const user = await this.usersService.findById(file.userId);
+      const userOwner = await this.usersService.findById(file.userId);
+      console.log({ userId: file.userId });
 
       const result = [];
       const xlsxData = await xlsx.readFile(path.join(filePath, file.file));
@@ -56,7 +57,7 @@ export class FileConsumer {
           userDto.fio = item.fio;
           userDto.email = item.email;
           userDto.group = item.group;
-          userDto.companyId = user.companyId;
+          userDto.companyId = userOwner?.companyId;
 
           const errors = await validate(userDto);
           if (!errors.length) {
